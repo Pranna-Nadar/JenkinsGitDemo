ee6c3eb078f18b91adf11a84a815f997ed582e19
Pranna_Nadar <prannanadar9@gmail.com>
2026-03-09 07:20:59 +0530

    Add greeting message for 'Hello Myself Pranna-MCA25065'
    Added a new print statement for 'Hello Myself Pranna-MCA25065' and retained the existing statement.

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
        System.out.println("Hello from GitHub - Poll SCM Working");
        System.out.println("Hello from Ngrok");
-        System.out.println("Hello from Ngrok Edit");
+       System.out.println("Hello from Ngrok Edit");
+       System.out.println("Hello Myself Pranna-MCA25065"); 
     }
 }
